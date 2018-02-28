b9ebb0ff6b1126c8f74cd432bd014a131a72f4c4
Sabrina Fiester <sabrina.fiester@gmail.com>
2018-02-27 19:57:11 -0600

    Update dependencies

--- server.js
+++ server.js
@@ -3,14 +3,9 @@
 // set up ========================
 var express  = require('express');
 var app      = express();                               // create our app w/ express
-var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
 
 // configuration =================
 app.use(express.static(__dirname + '/solution'));                 // set the static files location /solution/img will be /img for users
-
-// Body parser middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
 
 // application -------------------------------------------------------------
 app.get('*', function(req, res) {
